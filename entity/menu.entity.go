dedf66bbb8f6e51950e3538751fdec5f3da6397a
axlMax <axlyeyo2000@gmail.com>
2024-10-28 02:58:48 -0500

    add and test the create method

--- entity/menu.entity.go
+++ entity/menu.entity.go
@@ -5,14 +5,14 @@ func (Menu) TableName() string {
 }
 
 type Menu struct {
-	ItemID      uint   `gorm:"primaryKey;column:item_id" json:"item_id"`
-	Nombre      string `gorm:"column:nombre" json:"nombre"`
-	ItemPadreId *uint  `gorm:"column:item_padre_id" json:"item_padre_id,omitempty"`
-	RecursoID   uint   `gorm:"column:recurso_id" json:"recurso_id"`
-	EntidadID   uint   `gorm:"column:entidad_id" json:"entidad_id"`
+	MenuID uint   `gorm:"primaryKey;autoIncrement;column:menu_id"`
+	Nombre string `gorm:"column:nombre" json:"nombre"`
+	//ItemPadreId *uint  `gorm:"column:item_padre_id" json:"item_padre_id,omitempty"`
+	RecursoID   *uint  `gorm:"column:recurso_id" json:"recurso_id,omitempty"`
+	EntidadID   *uint  `gorm:"column:entidad_id" json:"entidad_id,omitempty"`
 	Descripcion string `gorm:"column:descripcion" json:"descripcion"`
 
-	ItemPadre *Menu    `gorm:"foreignKey:ItemPadreID;references:ItemID" json:"item_padre,omitempty"`
-	Recurso   Resource `gorm:"foreignKey:RecursoID" json:"recurso"`
-	Entidad   Entity   `gorm:"foreignKey:EntidadID" json:"entidad"`
+	//ItemPadre *Menu     `gorm:"foreignKey:ItemPadreID;references:ItemID" json:"item_padre,omitempty"`
+	Recurso *Resource `gorm:"foreignKey:RecursoID" json:"recurso,omitempty"`
+	Entidad *Entity   `gorm:"foreignKey:EntidadID" json:"entidad,omitempty"`
 }
