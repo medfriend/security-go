9ea9d8734a68d2974a6795b740342947e7768207
axlMax <axlyeyo2000@gmail.com>
2024-10-27 22:37:20 -0500

    create the entity

--- entity/menu.entity.go
+++ entity/menu.entity.go
@@ -1,4 +1,18 @@
 package entity
 
+func (Menu) TableName() string {
+	return "menu"
+}
+
 type Menu struct {
+	ItemID      uint   `gorm:"primaryKey;column:item_id" json:"item_id"`
+	Nombre      string `gorm:"column:nombre" json:"nombre"`
+	ItemPadreId *uint  `gorm:"column:item_padre_id" json:"item_padre_id,omitempty"`
+	RecursoID   uint   `gorm:"column:recurso_id" json:"recurso_id"`
+	EntidadID   uint   `gorm:"column:entidad_id" json:"entidad_id"`
+	Descripcion string `gorm:"column:descripcion" json:"descripcion"`
+
+	ItemPadre *Menu    `gorm:"foreignKey:ItemPadreID;references:ItemID" json:"item_padre,omitempty"`
+	Recurso   Resource `gorm:"foreignKey:RecursoID" json:"recurso"`
+	Entidad   Entity   `gorm:"foreignKey:EntidadID" json:"entidad"`
 }
