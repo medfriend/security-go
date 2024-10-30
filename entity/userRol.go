f7d5e1912e5f72ff221d0a59a727d9a2c5291488
axlMax <axlyeyo2000@gmail.com>
2024-10-30 00:00:12 -0500

    add userRol entity

---
+++ entity/userRol.go
@@ -0,0 +1,13 @@
+package entity
+
+import "time"
+
+type UserRol struct {
+	UsuarioRolID uint      `gorm:"primaryKey;autoIncrement;column:re-usuario-rol_id"`
+	UsuarioID    *uint     `gorm:"column:usuario_id" json:"usuario_id"`
+	RolID        *uint     `gorm:"column:rol_id" json:"rol_id"`
+	EntidadID    *uint     `gorm:"column:entidad_id" json:"entidad_id"`
+	FechaInicio  time.Time `gorm:"column:fecha_inicio" json:"fecha_inicio"`
+	FechaFin     time.Time `gorm:"column:fecha_fin" json:"fecha_fin"`
+	Indefinido   string    `gorm:"column:indefinido" json:"indefinido"`
+}
