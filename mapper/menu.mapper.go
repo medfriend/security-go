337915faaa54a882185f48e7a7cedc45590d9213
axlMax <axlyeyo2000@gmail.com>
2024-11-22 22:50:13 -0500

    eliminar comentarios

--- mapper/menu.mapper.go
+++ mapper/menu.mapper.go
@@ -1,14 +1,11 @@
 package mapper
 
 import (
-	"fmt"
 	"security-go/entity"
 	"security-go/response"
 )
 
 func MenuToMenuResponse(menu entity.Menu, hasSubmenu bool, permissions map[uint][]string) *response.MenuResponse {
-
-	fmt.Println("entre al mapper")
 
 	// Crear un `MenuResponse` básico
 	menuResponse := &response.MenuResponse{
@@ -20,20 +17,15 @@ func MenuToMenuResponse(menu entity.Menu, hasSubmenu bool, permissions map[uint]
 		MenuPadreId: menu.MenuPadreID,
 	}
 
-	fmt.Println("paso 1")
-
 	// Solo asignar `Recurso` si está disponible
 	if menu.Recurso != nil {
 		menuResponse.Recurso = menu.Recurso.Acceso
 		menuResponse.RecursoID = menu.Recurso.ResourceID
 
 		if permissions[menu.Recurso.ResourceID] != nil {
-			//menuResponse.Permissions = permissions[menu.Recurso.ResourceID]
+			menuResponse.Permissions = permissions[menu.Recurso.ResourceID]
 		}
-
 	}
-
-	fmt.Println("paso 2")
 
 	if hasSubmenu && menu.MenuPadre != nil {
 		// Crear una respuesta que representa al `MenuPadre`
@@ -45,8 +37,6 @@ func MenuToMenuResponse(menu entity.Menu, hasSubmenu bool, permissions map[uint]
 			Submenus:    []*response.MenuResponse{},
 		}
 	}
-
-	fmt.Println("paso 3")
 
 	return menuResponse
 }
